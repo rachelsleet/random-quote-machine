6c457e2353005f95f4625a6e2d97833cee34e991
Rachel Sleet <rachelsleet@gmail.com>
2019-03-16 21:21:04 +0100

    Updates

---
+++ precache-manifest.035aed2e3a01692b8d950b52dc42b0ff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6fefced6fd426e504746",
+    "url": "/random-quote-machine/static/js/runtime~main.296aa1cf.js"
+  },
+  {
+    "revision": "ff2a5c47deee8101ba7f",
+    "url": "/random-quote-machine/static/js/main.af7515b3.chunk.js"
+  },
+  {
+    "revision": "b43ba2d76bdc02655c76",
+    "url": "/random-quote-machine/static/js/2.67b98335.chunk.js"
+  },
+  {
+    "revision": "ff2a5c47deee8101ba7f",
+    "url": "/random-quote-machine/static/css/main.330c8b85.chunk.css"
+  },
+  {
+    "revision": "fbae8c10b59edda5f8f4bfffb377f133",
+    "url": "/random-quote-machine/index.html"
+  }
+];
